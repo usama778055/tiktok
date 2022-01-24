5d5b6d32c59204feceab63f216214c68b37e0ba7
Usama imran <usamaimran309@gmail.com>
2022-01-24 14:55:15 +0500

    change the purchase page

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="<?php echo base_url('assets/css/uikit.min.css') ?>" />
   <link href="<?php echo base_url('assets/css/main.css'); ?>" rel="stylesheet" type="text/css">
+  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </head>
 
 <body>

4b71beae44588923a6833568ce234d4921102f9e
M Kashif Raza <kashifraza8999@gmail.com>
2022-01-27 16:01:09 +0500

    added favicon

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -4,7 +4,7 @@
   <title>TikTok</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="<?php echo base_url('favicon')?>" />
+  <link rel="icon" href="<?php echo base_url('favicon.icon')?>" />
   <link rel="stylesheet" href="<?php echo base_url('assets/css/uikit.min.css') ?>" />
   <link href="<?php echo base_url('assets/css/main.css'); ?>" rel="stylesheet" type="text/css">
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />

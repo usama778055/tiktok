6ad54453a62af3723f0256b1b11665372c8053ef
Usama imran <usamaimran309@gmail.com>
2022-01-27 15:56:48 +0500

    changing

--- application/views/package/purchase_package.php
+++ application/views/package/purchase_package.php
@@ -1,129 +1,129 @@
 
-	<?php $this->load->view('layouts/header'); ?>
-	<div class="uk-section feed-banner custom-padding-top">
-		<div class="uk-container">
-			<h6>Checkout</h6>
-			<h3>Buy <?php echo $user_data[0]->packageQty.' '.'TikTok'.' '.$user_data[0]->serviceType; ?></h3>
-		</div>
+<?php $this->load->view('layouts/header'); ?>
+<div class="uk-section feed-banner custom-padding-top">
+	<div class="uk-container">
+		<h6>Checkout</h6>
+		<h3>Buy <?php echo $user_data[0]->packageQty.' '.'TikTok'.' '.$user_data[0]->serviceType; ?></h3>
 	</div>
+</div>
 
 
-	<!--------------------Feed-main-sec----------------->
-	<div class="feed-main uk-section">
-		<div class="uk-container">
-			<div class="" uk-grid>
-				<div class="uk-width-1-2@s">
-					<div class="left-feed-sec">
-						<div class="left-feed-upper">
+<!--------------------Feed-main-sec----------------->
+<div class="feed-main uk-section">
+	<div class="uk-container">
+		<div class="" uk-grid>
+			<div class="uk-width-1-2@s">
+				<div class="left-feed-sec">
+					<div class="left-feed-upper">
 
-							<form class="" uk-grid>
-								<div class="uk-width-1-2@m">
-									<label class="uk-form-label" for="form-stacked-text">Your Tiktok User ID</label>
-									<div class="uk-form-controls">
-										<input class="uk-input user_name" id="form-stacked-text" type="text" placeholder="@therock" onchange="from_get_api()">
-									</div>
+						<form class="" uk-grid>
+							<div class="uk-width-1-2@m">
+								<label class="uk-form-label" for="form-stacked-text">Your Tiktok User ID</label>
+								<div class="uk-form-controls">
+									<input class="uk-input user_name" id="form-stacked-text" type="text" placeholder="@therock" onchange="from_get_api(event)">
 								</div>
-								<div class="uk-width-1-2@m custom-selected" uk-form-custom="target: > * > span:first-child">
-									<label for="" class="uk-form-label">Package Selected</label>			<select class="js-example-basic-single" onchange="myFunction()">
-										<?php foreach($alldata as $values){
-
-											if($user_data[0]->id == $values->id){
-												$att = 'selected';
-											}
-											else{
-												$att = '';
-											}
-											?>
-
-											<option value="<?php echo base_url('buy-'.$values->packageQty.'-tiktok-'.$values->serviceType) ?>" class="" <?php echo $att; ?>><?php echo $values->packageQty.' '.$values->packageTitle; ?></option>
-										<?php } ?>
-									</select>
-									<button class="uk-button uk-button-default" type="button" tabindex="-1">
-										<span></span>
-										<span uk-icon="icon: chevron-down"></span>
-									</button>
-								</div>
-							</form>
-
-						</div>
-						<div class="left-feed-botoom">
-
-							<div class="load-gallery custom_image_class">
-									
-								
 							</div>
-							<a id="loadmore" class="serv-btn" href="#"><span class="spanbtn">Load More</span></a>
+							<div class="uk-width-1-2@m custom-selected" uk-form-custom="target: > * > span:first-child">
+								<label for="" class="uk-form-label">Package Selected</label>			<select class="js-example-basic-single" onchange="myFunction()">
+									<?php foreach($alldata as $values){
+
+										if($user_data[0]->id == $values->id){
+											$att = 'selected';
+										}
+										else{
+											$att = '';
+										}
+										?>
+
+										<option value="<?php echo base_url('buy-'.$values->packageQty.'-tiktok-'.$values->serviceType) ?>" class="" data_id="<?php echo $values->packageQty;?>" <?php echo $att; ?>><?php echo $values->packageQty.' '.$values->packageTitle; ?></option>
+									<?php } ?>
+								</select>
+								<button class="uk-button uk-button-default" type="button" tabindex="-1">
+									<span></span>
+									<span uk-icon="icon: chevron-down"></span>
+								</button>
+							</div>
+						</form>
+
+					</div>
+					<div class="left-feed-botoom">
+
+						<div class="load-gallery custom_image_class">
+							<div class="loader_class" uk-spinner style="display: none; color: white;"></div>		
+							
 						</div>
+						<a id="loadmore" class="serv-btn" href="#"><span class="spanbtn">Load More</span></a>
 					</div>
 				</div>
-				<div class="uk-width-1-2@s">
-					<div class="right-feed-sec">
-						<div class="invoice-modal">
-							<h4>Invoice</h4>
-							<div class="user-img">
-								<img src="images/Rectangle53.PNG">
-								<div class="">
-									<h5>The Rock</h5>
-									<p>@therock</p>
-								</div>
+			</div>
+			<div class="uk-width-1-2@s">
+				<div class="right-feed-sec">
+					<div class="invoice-modal">
+						<h4>Invoice</h4>
+						<div class="user-img">
+							<img height=100 width=100 src="<?php echo base_url('assets/images/placeholder_person.png') ?>">
+							<div class="">
+								<h5>The Rock</h5>
+								<p>@therock</p>
 							</div>
-							<div class="order">
-								<h5>Order</h5>
-								<p><?php echo $user_data[0]->packageQty .' '. $user_data[0]->serviceType ?></p>
-							</div>
-							<div class="price">
-								<h5>Price</h5>
-								<p><?php echo $user_data[0]->priceUnit .' '. $user_data[0]->packagePrice ?></p>
-							</div>
-							<div class="promo-code">
-								<h5>Have a promo code?</h5>
-								<input class="uk-input" id="form-stacked-text" type="text" placeholder="apply here">
-							</div>
-							<div class="total">
-								<h5>Total</h5>
-								<p><?php echo $user_data[0]->priceUnit .' '. $user_data[0]->packagePrice ?></p>
-							</div>
-							<div class="invoice-modal-btn">
-								<a href="">Add To Cart</a>
-								<a class="active" href="">Checkout</a>
-							</div>
+						</div>
+						<div class="order">
+							<h5>Order</h5>
+							<p><?php echo $user_data[0]->packageQty .' '. $user_data[0]->serviceType ?></p>
+						</div>
+						<div class="price">
+							<h5>Price</h5>
+							<p><?php echo $user_data[0]->priceUnit .' '. $user_data[0]->packagePrice ?></p>
+						</div>
+						<div class="promo-code">
+							<h5>Have a promo code?</h5>
+							<input class="uk-input" id="form-stacked-text" type="text" placeholder="apply here">
+						</div>
+						<div class="total">
+							<h5>Total</h5>
+							<p><?php echo $user_data[0]->priceUnit .' '. $user_data[0]->packagePrice ?></p>
+						</div>
+						<div class="invoice-modal-btn">
+							<a href="">Add To Cart</a>
+							<a class="active" href="">Checkout</a>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<!-------------------------------------------------->
+</div>
+<!-------------------------------------------------->
 
-	<!------------------feed-sec-packages--------------->
-	<div class="feed-sec-packages uk-section">
-		<div class="uk-container uk-width-4-5@s"uk-slider>
-			<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
-				<h6>Add on</h6>
-				<h4>Take a look at these amazing packages</h4>
-				<ul class="uk-slider-items uk-child-width-1-3@m uk-child-width-1-2@s uk-grid">
+<!------------------feed-sec-packages--------------->
+<div class="feed-sec-packages uk-section">
+	<div class="uk-container uk-width-4-5@s"uk-slider>
+		<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
+			<h6>Add on</h6>
+			<h4>Take a look at these amazing packages</h4>
+			<ul class="uk-slider-items uk-child-width-1-3@m uk-child-width-1-2@s uk-grid">
 					<?php foreach ($featured as $key => $value) {// code...
-					$parts = explode('.', $value->packagePrice);
-					?>
-					<li class="feed-box">
-						<div class="amazing-package">
-							<h5>Buy <?php echo $value->packageQty ?> Tiktok <?php echo $value->serviceType; ?></h5>
-							<small><?php echo $value->priceUnit.' '.$parts[0]; ?><sup><?php if(!empty($parts[1])){
-								echo $parts[1];
-							}
-							else{
+						$parts = explode('.', $value->packagePrice);
+						?>
+						<li class="feed-box">
+							<div class="amazing-package">
+								<h5>Buy <?php echo $value->packageQty ?> Tiktok <?php echo $value->serviceType; ?></h5>
+								<small><?php echo $value->priceUnit.' '.$parts[0]; ?><sup><?php if(!empty($parts[1])){
+									echo $parts[1];
+								}
+								else{
 
-							}?></sup></small>
-							<ul class="uk-list">
-								<li>High Quality</li>
-								<li>Active and Real Users </li>
-								<li>Instant Delivery</li>
-								<li>24/7 Support</li>
-							</ul>
-							<a class="serv-btn" href="<?php echo base_url('buy-'.$value->packageQty.'-tiktok-'.$value->serviceType); ?>"><span class="spanbtn">Purchase</span></a>
-						</div>
-					</li>
-				<?php } ?>
+								}?></sup></small>
+								<ul class="uk-list">
+									<li>High Quality</li>
+									<li>Active and Real Users </li>
+									<li>Instant Delivery</li>
+									<li>24/7 Support</li>
+								</ul>
+								<a class="serv-btn" href="<?php echo base_url('buy-'.$value->packageQty.'-tiktok-'.$value->serviceType); ?>"><span class="spanbtn">Purchase</span></a>
+							</div>
+						</li>
+					<?php } ?>
 				</ul>
 			</div>
 			<ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -96,72 +96,142 @@ $('.package_button').bind("click",function () {
         });
 });
 
-
-
-function from_get_api() {
-    var option = $(this).find('selected');
-    console.log(option);
-    return;
-    $(this).keypress(function (event) {
-        if (event.keyCode == 13) {
-            event.preventDefault();
-        }
-    });
-    var get_value = $('.user_name').val();
-    if(get_value === ''){
-        console.log('usama');
-        UIkit.notification({message: 'Field empty', pos: 'top-right',status:'danger'});
+jQuery(document).on("keypress", 'input', function (e) {
+    var code = e.keyCode || e.which;
+    if (code === 13) {
+        e.preventDefault();
         return false;
     }
-    $.ajax({
-        method:"post",
-        url : base_url+"get_tiktokuser_data",
-        data : { "name": get_value },
-        success : function(response){
+});
 
-            var result= $.parseJSON(response);
-            var images = result.post_links;
-            var html = '';
-            $.each(images , function( index, value ) {
-                html += "<div>"+
-                "<div class='gallery-image uk-transition-toggle selected_div' tabindex='0'>"+
-                "<img class='uk-transition-scale-up uk-transition-opaque' src='"+value+"'>"+
-                "<div class='uk-position-bottom uk-overlay-default get_select' style='display: none;' data_id='get_select'>"+
-                "<p class='uk-h4 uk-margin-remove'>200</p>"+
-                "</div>"+
-                "</div>"+
-                "</div>"; 
-                $('.custom_image_class').prepend(html);
+$( "#form-stacked-text" ).change(function() {
+
+   
+   var getclass = $('.js-example-basic-single').find(':selected').attr('data_id');
+
+
+   var get_value = $('.user_name').val();
+   var find = $(this).find('.user_name');
+   if(get_value === ''){
+
+    UIkit.notification({message: 'Field empty', pos: 'top-right',status:'danger'});
+    return false;
+}
+$.ajax({
+    method:"post",
+    url : base_url+"get_tiktokuser_data",
+    data : { "name": get_value },
+    beforeSend: function() {
+        $(".loader_class").show();
+        $(this).prop('disabled', true);
+    },
+    success : function(response){
+        $(".loader_class").hide();
+        $(this).prop('disabled', false);
+        
+        var result= $.parseJSON(response);
+        var images = result.post_links;
+        var len = images.length;
+        var html = '';
+        var count = 1;
+            /*const quantity = packageQty / 1;
+            const service_limit = limit / 1;*/
+            $.each(images,function(index, post){
+                console.log(post);
+                
+                html += `<div data_id=''>
+                <div class='gallery-image uk-transition-toggle selected_div' tabindex='0'>
+                <img class='uk-transition-scale-up uk-transition-opaque' src='${post}'>
+                <div class='uk-position-bottom uk-overlay-default get_select' style=''>
+                <p class='uk-h4 uk-margin-remove putquentity'></p>
+                </div>
+                </div>
+                </div>`; 
+                
+                /*if(i === 0){
+                console.log(issue);
+                }
+                else{
+                issue = -getclass / 2 + issue;
+                console.log(issue);
+                
+                
+            }*/
+
+        });
+            $('.custom_image_class').html(html);
+            if ($('.gallery-image:hidden').length !== 0) {
+                $('#loadmore').show();
+            }
+
+            $('.gallery-image').slice(0, 4).show();
+            $('#loadmore').on('click', function (e) {
+                e.preventDefault();
+                $('.gallery-image:hidden').slice(0, 1).slideDown();
+                if ($('.gallery-image:hidden').length === 0) {
+                    $('#loadmore').hide();
+                }
+                else{
+                    
+                }
                 
             });
-            $('.gallery-image').slice(0, 10).show();
-                $('#loadmore').on('click', function (e) {
-                    e.preventDefault();
-                    $('.gallery-image:hidden').slice(0, 4).slideDown();
-                    if ($('.gallery-image:hidden').length === 0) {
-                        $('#loadmore').replaceWith("<a id='nomore-image' class='serv-btn' href=''><span class='spanbtn'>No More</span></a>");
-                    }
-                });
 
 
-        }
+        }   
     });
-}
+});
 
 
 
 $(document).on('click', ".selected_div", function () {
-    if($(this).hasClass("selected"))
-    {
-        $(this).removeClass("selected");
-        $(this).find('.get_select').hide();
-        return false;
-    }
+    $(this).toggleClass("selected");
+    /*if ($(this).hasClass("selected")) {
+      $(this).removeClass("selected");  
+  }*/
+  /*   $(this).find('.get_select').show();*/
+  var length = $('.selected_div.selected').length;
+  console.log(length);
+  var limit = 50;
+  var packageQty = $('.js-example-basic-single').find(':selected').attr('data_id');
+  const quantity = packageQty / 1;
+  const service_limit = limit / 1;
+  let per_input = Math.floor(quantity / length);
 
-    $('.selected_div').addClass("selected");
-    $(this).find('.get_select').show();
-    
-});
+  const remaining = quantity % length;
+  if (length > 1 && per_input < 50){
+    /*$(this).addClass("shake animated");*/
+    console.log(per_input);
+    $(this).removeClass("selected");
+    $(this).find(".putquentity").text('');
+
+    console.log('error');
+
+            /*setTimeout(() => {
+                $(this).removeClass("shake animated");
+            }, 600);*/
+            return false;
+        }
+
+        $(".selected_div").each((index, post) => {
+
+            if ($(post).hasClass("selected")) {
+                if (index === length - 1) {
+                    per_input += remaining;
+                }
+                $(post).find(".putquentity").text(per_input);
+                /*$(post).find(".per_quantity").val(per_input);*/
+            } else {
+                $(post).find(".putquentity").text("");
+                /*$(post).find(".per_quantity").val(0);*/
+            }
+        });
+
+
+        /*console.log(select);*/
+
+
+    });
 
 
 function myFunction() {
@@ -183,7 +253,7 @@ function myFunction() {
 
             }
         });
-});*/
+    });*/
 
 
 

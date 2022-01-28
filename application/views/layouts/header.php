8c4ce55dc67e4874e35f53316652e08a30152b9a
M Kashif Raza <kashifraza8999@gmail.com>
2022-01-28 13:19:28 +0500

    added static checkout view, corrected faqs styles to make them prominent

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -66,8 +66,8 @@
                       </tbody>
                     </table>
                     <div class="cart-btn">
-                      <a href="">I want to shop more</a>
-                      <a class="payment-btn" href="">Proceed To Payment</a>
+                      <a href="<?= base_url() ?>">I want to shop more</a>
+                      <a class="payment-btn" href="<?= base_url('checkout') ?>">Proceed To Payment</a>
                     </div>
                   </div>
                 </div>

9d87f4e2510a1fd82625ea7d2879b380fb4c3eb8
Usama imran <usamaimran309@gmail.com>
2022-02-10 11:18:56 +0500

    api_images_partial_view

--- application/views/package/api_images_partial_view.php
+++ application/views/package/api_images_partial_view.php
@@ -2,15 +2,10 @@
 $i=1;
 foreach ($user['post_links'] as $key => $value): ?>
     
-<<<<<<< HEAD
-    <div>
-    <div class='gallery-image uk-transition-toggle selected_div' tabindex='0'>
-        <img id="img_api_<?php echo $key; ?>" data_id='<?php echo $key; ?>' class='uk-transition-scale-up uk-transition-opaque' src='<?php print_r($value); ?>'>
-=======
+
     <div data_id='<?php echo $i?>'>
     <div class='gallery-image uk-transition-toggle selected_div' tabindex='0'>
         <img class='uk-transition-scale-up uk-transition-opaque' src='<?php echo $value; ?>'>
->>>>>>> ed27085132ab94808ef19465532a2b7ac04e126f
         <div class='uk-position-bottom uk-overlay-default get_select' style=''>
             <p id='quenty_<?php echo $key; ?>' class='uk-h4 uk-margin-remove putquentity'></p>
         </div>
